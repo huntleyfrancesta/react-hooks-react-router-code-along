1e4f72d00ecdb1b1c9956dc73fabbee1fb692fab
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-05-23 22:41:39 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function Home() {
   return (
+
+  )
+
+  };
+
+
+function About() {
+  return (
     <div>
-      <h1>Home!</h1>
+      <h1>This is my about component!</h1>
     </div>
   );
 }
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
+
+
+ReactDOM.render(
+  <Router>
+    <Switch>    
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
